14a711eef8573ac7acaa11f5aa3a767acd995b06
ummer-shell <124354705+ummer-shell@users.noreply.github.com>
2023-04-13 13:22:58 +0000

    Update chrome driver options

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -32,7 +32,7 @@ def start_browser():
     options = ChromeOptions()
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    chromeOptions.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--headless")
 
     driver = webdriver.Chrome(options=options)
