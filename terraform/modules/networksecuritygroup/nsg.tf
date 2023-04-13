4a36b9cc9ddfe9a49fe896ed8fe5fcc1d8bcd155
ummer-shell <124354705+ummer-shell@users.noreply.github.com>
2023-04-13 09:24:36 +0000

    Update network security rules

--- terraform/modules/networksecuritygroup/nsg.tf
+++ terraform/modules/networksecuritygroup/nsg.tf
@@ -3,28 +3,28 @@ resource "azurerm_network_security_group" "nsg" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
 
-  security_rule {
-    name                       = "${var.application_type}-${var.resource_type}-5000"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "5000"
-    source_address_prefix      = "${var.address_prefix_test}"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+  # security_rule {
+  #   name                       = "${var.application_type}-${var.resource_type}-5000"
+  #   priority                   = 100
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "5000"
+  #   source_address_prefix      = "${var.address_prefix_test}"
+  #   destination_address_prefix = "*"
+  # }
+  # security_rule {
+  #       name                       = "SSH"
+  #       priority                   = 1001
+  #       direction                  = "Inbound"
+  #       access                     = "Allow"
+  #       protocol                   = "Tcp"
+  #       source_port_range          = "*"
+  #       destination_port_range     = "22"
+  #       source_address_prefix      = "*"
+  #       destination_address_prefix = "*"
+  #   }
 
   security_rule {
       name                       = "allow-access"
