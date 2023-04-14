76f9b9da4649ec97ccc36e037fff200073ace379
ummer-shell <124354705+ummer-shell@users.noreply.github.com>
2023-04-14 09:05:34 +0000

    Update app service plan to windows

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,7 +2,7 @@ resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = "S1"
 }
 
